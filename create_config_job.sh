d2bf642facc9781fa3c5f7420016eb5bdfa6c819
evangowan <evangowan@gmail.com>
2017-07-27 16:50:09 +0200

    changed the mode of the scripts so they are executable

--- create_config_job.sh
+++ create_config_job.sh
@@ -1,11 +1,25 @@
 #! /bin/bash
 #SBATCH --time=03:00:00
-#SBATCH --ntasks=1
 #SBATCH --mem=10G
+#SBATCH --nodes=1              # Can be skipped (default)
+#SBATCH --ntasks-per-node=1    # Can be skipped (default)
+#SBATCH --cpus-per-task=8     # xx Threads, Slurm sets OMP_NUM_THREADS accordingly
+#SBATCH -p smp
 #SBATCH --output=selen_config.out
 #SBATCH --job-name=selen_config
-LANG=C
+
+
+# For Intel compiled code, sets path to OpenMP lib. Change for Cray compiler.
+module load intel.compiler
+
+##  Enlarge the stacksize, just to be on the safe side.
 ulimit -s unlimited
+
+# This binds each thread to one core
+export OMP_PROC_BIND=TRUE
+
+srun /global/AWIsoft/xthi/xthi.intel    # just to check the thread-CPU-distribution
+
 
 cd /work/ollie/egowan/icesheet/selen
 echo "SLURM_JOBID:         $SLURM_JOBID"
